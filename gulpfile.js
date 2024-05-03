ad2382a8e79f17b3dcb2c616617ec998b31034f4
agj <ale@agj.cl>
2024-05-02 20:17:24 -0400

    Remove unused stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 import gulp from "gulp";
 import elm from "gulp-elm";
 import rename from "gulp-rename";
-import { promisify } from "util";
-import { exec as exec_ } from "child_process";
 import path from "path";
 
 import retrieveWorks from "./source/js/retrieve-works.js";
