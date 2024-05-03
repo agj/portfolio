3e922b833faa9755cb585dda4b86acef415f9bb6
agj <ale@agj.cl>
2024-05-02 20:41:44 -0400

    Fix files copied with errors

--- gulpfile.js
+++ gulpfile.js
@@ -44,13 +44,18 @@ const developElm = () =>
 // Static files copy
 
 const copyGeneralData = () =>
-  gulp.src(path.join(cfg.copyDir, "**")).pipe(gulp.dest(cfg.outputDir));
+  gulp
+    .src(path.join(cfg.copyDir, "**"), { encoding: false })
+    .pipe(gulp.dest(cfg.outputDir));
 const copyCache = () =>
   gulp
-    .src([
-      path.join(cfg.cacheDir, "**/*.*"),
-      path.join(`!${cfg.cacheDir}`, "**/*.meta.json"),
-    ])
+    .src(
+      [
+        path.join(cfg.cacheDir, "**/*.*"),
+        path.join(`!${cfg.cacheDir}`, "**/*.meta.json"),
+      ],
+      { encoding: false }
+    )
     .pipe(gulp.dest(path.join(cfg.outputDir, "works/")));
 
 const copy = gulp.parallel(copyGeneralData, copyCache);
