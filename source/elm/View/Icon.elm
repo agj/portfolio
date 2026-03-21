03c9a46d79ddba995def4defb01e4f322d02b002
agj <ale@agj.cl>
2026-03-21 20:26:08 -0300

    Add ability to set icon style

--- source/elm/View/Icon.elm
+++ source/elm/View/Icon.elm
@@ -1,14 +1,24 @@
 module View.Icon exposing
     ( Icon
     , IconName(..)
+    , Style(..)
     , icon
     , view
+    , withStyle
     )
 
 import Element exposing (Element)
 import Element.Font
 import Html.Attributes
 import Phosphor
+
+
+type Icon
+    = Icon
+        { name : IconName
+        , size : Int
+        , style : Style
+        }
 
 
 type IconName
@@ -24,23 +34,26 @@ type IconName
     | ArrowLeft
 
 
-type Icon
-    = Icon
-        { name : IconName
-        , size : Int
-        }
+type Style
+    = StyleFilled
+    | StyleStroke
 
 
 icon : IconName -> Int -> Icon
 icon name size =
-    Icon { name = name, size = size }
+    Icon { name = name, size = size, style = StyleFilled }
+
+
+withStyle : Style -> Icon -> Icon
+withStyle style (Icon config) =
+    Icon { config | style = style }
 
 
 view : Icon -> Element msg
-view (Icon { name, size }) =
+view (Icon config) =
     let
         phosphorIcon =
-            case name of
+            case config.name of
                 Language ->
                     Phosphor.chatCircle
 
@@ -70,12 +83,19 @@ view (Icon { name, size }) =
 
                 ArrowLeft ->
                     Phosphor.arrowLeft
+
+        style =
+            case config.style of
+                StyleFilled ->
+                    Phosphor.Fill
+
+                StyleStroke ->
+                    Phosphor.Bold
     in
-    phosphorIcon Phosphor.Fill
+    phosphorIcon style
         |> Phosphor.toHtml
             [ Html.Attributes.style "vertical-align" "middle"
             ]
         |> Element.html
         |> Element.el
-            [ Element.Font.size size
-            ]
+            [ Element.Font.size config.size ]
