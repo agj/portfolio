450888935a00e5ce9450889ef5348ac6f399d700
agj <ale@agj.cl>
2026-04-17 18:51:28 -0400

    Fix incorrect namespace definition

--- source/elm/declarations.d.ts
+++ source/elm/declarations.d.ts
@@ -1,30 +1,30 @@
-namespace Main {
-  const init = (options: { node?: HTMLElement | null; flags: Flags }) => ElmApp;
-}
-
-type Flags = unknown;
-
-type ElmApp = {
-  ports: {
-    saveState: PortFromElm<string>;
-    scrollTo: PortFromElm<string>;
-    getViewport: PortFromElm<unknown>;
-    gotViewport: PortToElm<unknown>;
-    scrolledOverWorkPort: PortToElm<unknown>;
-  };
-};
-
-type PortFromElm<Data> = {
-  subscribe(callback: (fromElm: Data) => void): void;
-};
-
-type PortToElm<Data> = {
-  send(data: Data): void;
-};
-
 // Don't know how to more specifically declare types for this one module, the
 // obvious options didn't work.
 // Ref: https://www.typescriptlang.org/docs/handbook/modules/reference.html
 declare module "*/Main.elm" {
+  namespace Main {
+    function init(options: { node?: HTMLElement | null; flags: Flags }): ElmApp;
+  }
+
+  type Flags = unknown;
+
+  type ElmApp = {
+    ports: {
+      saveState: PortFromElm<string>;
+      scrollTo: PortFromElm<string>;
+      getViewport: PortFromElm<unknown>;
+      gotViewport: PortToElm<unknown>;
+      scrolledOverWorkPort: PortToElm<unknown>;
+    };
+  };
+
+  type PortFromElm<Data> = {
+    subscribe(callback: (fromElm: Data) => void): void;
+  };
+
+  type PortToElm<Data> = {
+    send(data: Data): void;
+  };
+
   export default Main;
 }
